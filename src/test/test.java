233a6cdd09e6cb6150a6cfb0a0ad19a9d2c14cde
imaltont <sondrehegdal@hotmail.com>
2014-02-27 16:55:21 +0100

    WOHO!

---
+++ src/test/test.java
@@ -0,0 +1,15 @@
+package test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Sondre Hegdal
+ * Date: 27.02.14
+ * Time: 16:53
+ * To change this template use File | Settings | File Templates.
+ */
+public class test {
+    public static void main (String [] args)
+    {
+        System.out.print ("WOHO!!");
+    }
+}
