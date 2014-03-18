9d684a453d87be5233378e00c6c59910dd7db5fc
SondrePadoy <sondre.padoey@live.com>
2014-03-18 13:03:01 +0100

    sdf

---
+++ src/no/ntnu/fp/model/Employee.java
@@ -0,0 +1,72 @@
+
+package no.ntnu.fp.model;
+
+import java.util.ArrayList;
+
+import no.ntnu.fp.storage.DatabaseHandler;
+
+public class Employee {
+	private String username;
+	private String password;
+	private String email;
+	private String name;
+	private ArrayList <Appointment> myAppointments;
+	private ArrayList <Appointment> invitedAppointments;
+	private ArrayList <Alarm> alarms;
+	
+
+	public Employee(String u, String p, String e, String n){
+		DatabaseHandler data = new DatabaseHandler();
+		data.addUser(u, p, e, n);
+		username=u;
+		password=p;
+		email=e;
+		name=n;
+		
+	}
+//Setters & getters
+	public ArrayList<Alarm> getAlarms() {
+		return alarms;
+	}
+	public void setAlarms(ArrayList<Alarm> alarms) {
+		this.alarms = alarms;
+	}
+	public String getEmail() {
+		return email;
+	}
+	public void setEmail(String email) {
+		this.email = email;
+	}
+	public String getUsername() {
+		return username;
+	}
+	public void setUsername(String username) {
+		this.username = username;
+	}
+	public String getPassword() {
+		return password;
+	}
+	public void setPassword(String password) {
+		this.password = password;
+	}
+	public String getName() {
+		return name;
+	}
+	public void setName(String name) {
+		this.name = name;
+	}
+	public ArrayList<Appointment> getMyAppointments() {
+		return myAppointments;
+	}
+	public void setMyAppointments(ArrayList<Appointment> myAppointments) {
+		this.myAppointments = myAppointments;
+	}
+	public ArrayList<Appointment> getInvitedAppointments() {
+		return invitedAppointments;
+	}
+	public void setInvitedAppointments(ArrayList<Appointment> invitedAppointments) {
+		this.invitedAppointments = invitedAppointments;
+	}
+
+}
+
